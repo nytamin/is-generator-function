1b277f1ac59c5c3ec8f7301d4349b72bfd56f661
Johan Nyman <johan@nytamin.se>
2026-02-24 08:09:09 +0100

    Fix import issue for generator-function
    Fix import for generator-function module.
    This is a hack to avoid a breaking change introduced in v1.1.1, #5477ff1

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ var fnToStr = callBound('Function.prototype.toString');
 
 var getGeneratorFunction = require('generator-function');
 
+// Hack to fix import, see https://github.com/inspect-js/is-generator-function/issues/45
+if (typeof getGeneratorFunction === 'object' && typeof getGeneratorFunction.default === 'function') getGeneratorFunction = getGeneratorFunction.default
+
 /** @type {import('.')} */
 module.exports = function isGeneratorFunction(fn) {
 	if (typeof fn !== 'function') {
@@ -26,6 +29,7 @@ module.exports = function isGeneratorFunction(fn) {
 	if (!getProto) {
 		return false;
 	}
+	
 	var GeneratorFunction = getGeneratorFunction();
 	return GeneratorFunction && getProto(fn) === GeneratorFunction.prototype;
 };
